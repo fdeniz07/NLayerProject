07d2ca71f5547b85c6f5b11519cd588bcf29bcf1
Fatih Deniz <deniz.fatih@hotmail.de>
2021-08-04 08:36:00 +0200

    Day 2

---
+++ NLayerProject.Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayerProject.Core.Model;
+
+namespace NLayerProject.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Burasi elimizdeki entity ler DB de tablolara dönüsürken nasil dönüsecegini belirliyoruz. Yani product ve category tablolari icerisindeki property'ler
+            //Db de tablolara yazilirken uzunlugu,türü,primarykey olup olmayacagi buradan belirlenir.
+        }
+    }
+}
